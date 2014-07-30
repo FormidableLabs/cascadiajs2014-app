3dd1d39f27bf863de82a93c1b81127800c4f9705
Ryan Roemer <ryan@loose-bits.com>
2014-07-30 15:48:54 -0700

    Add setup/teardown

--- test/mocha/js/spec/models/note.spec.js
+++ test/mocha/js/spec/models/note.spec.js
@@ -1,5 +1,15 @@
-define([], function () {
+define([
+  "app/models/note"
+], function (NoteModel) {
   describe("app/models/note", function () {
+
+    beforeEach(function () {
+      this.model = new NoteModel();
+    });
+    afterEach(function () {
+      this.model = null;
+    });
+
     it("should have expected defaults");
     it("can set the title property");
     it("can set the text property");
