cd88723b0377a7a321bfdf5e28ddf2952cd5c353
Ryan Roemer <ryan@loose-bits.com>
2014-07-30 15:45:14 -0700

    Add skeleton test structure.

---
+++ test/mocha/js/spec/models/note.spec.js
@@ -0,0 +1,7 @@
+define([], function () {
+  describe("app/models/note", function () {
+    it("should have expected defaults");
+    it("can set the title property");
+    it("can set the text property");
+  });
+});
